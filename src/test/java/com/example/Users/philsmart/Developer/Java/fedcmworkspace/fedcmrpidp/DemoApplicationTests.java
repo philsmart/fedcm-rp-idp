c3e757b58ca298701ccce67f0e6bb0d28c502e12
Phil Smart <philip.smart@jisc.ac.uk>
2023-01-11 21:32:38 +0000

    Add initial

---
+++ src/test/java/com/example/Users/philsmart/Developer/Java/fedcmworkspace/fedcmrpidp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Users.philsmart.Developer.Java.fedcmworkspace.fedcmrpidp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
